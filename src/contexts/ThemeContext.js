b0f8d3ac10c67ea101a2e824151c008810c5ae73
Ícaro Barboza <83622388+IcarusNB@users.noreply.github.com>
2022-03-22 09:00:10 -0300

    Initial commit

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,14 @@
+import React, { createContext } from 'react'
+import useTheme from '../hooks/useTheme'
+
+export const ThemeContext = createContext();
+
+export function ThemeProvider ({ children, startingTheme }) {
+  const { theme, setTheme } = useTheme(startingTheme)
+  
+  return (
+    <ThemeContext.Provider value={{ setTheme, theme }} >
+        {children}
+    </ThemeContext.Provider>
+  )
+}
